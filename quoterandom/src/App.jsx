43e20fbd4f2a937251e075c610c182f67fd1e12f
Busra-Demirkesen <busrademirkesen2@gmail.com>
2025-04-24 22:29:15 +0200

    refactor: clean up unused components and styles for better maintainability

--- quoterandom/src/App.jsx
+++ quoterandom/src/App.jsx
@@ -4,6 +4,8 @@ import QuoteCard from './components/QuoteCard/QuoteCard';
 import { useState, useEffect } from 'react';
 
 function App() {
+
+  
   const [quotes, setQuotes] = useState(() => {
     const saved = localStorage.getItem('quotes');
     return saved ? JSON.parse(saved) : initialQuotes;
@@ -15,10 +17,22 @@ function App() {
     localStorage.setItem('quotes', JSON.stringify(quotes));
   }, [quotes]);
 
+
+  const [history,setHistory] = useState([]);
+
   const handleNextQuoteClick = () => {
     const randomIndex = Math.floor(Math.random() * quotes.length);
+    setHistory(prev => [...prev, currentIndex]);
     setCurrentIndex(randomIndex);
   };
+
+  const handlePreviousQuoteClick = () => {
+    if (history.length === 0) return;
+
+    const previousIndex = history[history.length - 1];
+  setCurrentIndex(previousIndex);
+  setHistory(prev => prev.slice(0, prev.length - 1));
+  }
 
   const handleLikeClick = () => {
     const updatedQuotes = quotes.map((quote, index) => {
@@ -35,8 +49,13 @@ function App() {
       <QuoteCard quoteObj={quotes[currentIndex]} />
 
       <div className="btn-container">
-        <button onClick={handleLikeClick}>❤️</button>
-        <button onClick={handleNextQuoteClick}>Next Quote</button>
+      <button onClick={handleLikeClick}>
+  <i className="fa-solid fa-thumbs-up"></i>
+</button>
+
+        <button onClick={handlePreviousQuoteClick}><i class="fa-solid fa-left-long"></i></button>
+        <button onClick={handleNextQuoteClick}><i class="fa-solid fa-right-long"></i></button>
+       
       </div>
     </div>
   );
--- quoterandom/src/components/QuoteCard/QuoteCard.js
+++ quoterandom/src/components/QuoteCard/QuoteCard.js
@@ -5,7 +5,7 @@ function QuoteCard({ quoteObj }) {
     <div className="quote-card">
       <p className="quote-text">"{quoteObj.quote}"</p>
       <p className="quote-author">- {quoteObj.author}</p>
-      <p className="quote-likes">Likes: ❤️ {quoteObj.likeCount}</p>
+      <p className="quote-likes"><i className="fa-solid fa-heart"></i> {quoteObj.likeCount}</p>
     </div>
   );
 }
