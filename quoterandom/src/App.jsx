0e551da8522383641b6b56d3f04312efdd1f82dd
Busra-Demirkesen <busrademirkesen2@gmail.com>
2025-05-03 23:01:49 +0200

    refactor: improve code formatting and consistency in App component

--- quoterandom/src/App.jsx
+++ quoterandom/src/App.jsx
@@ -1,38 +1,34 @@
-import { quotes as initialQuotes } from './quotes';
-import './App.css';
-import QuoteCard from './components/QuoteCard/QuoteCard';
-import { useState, useEffect } from 'react';
+import { quotes as initialQuotes } from "./quotes";
+import "./App.css";
+import QuoteCard from "./components/QuoteCard/QuoteCard";
+import { useState, useEffect } from "react";
 
 function App() {
 
   
   const [quotes, setQuotes] = useState(() => {
-    const saved = localStorage.getItem('quotes');
+    const saved = localStorage.getItem("quotes");
     return saved ? JSON.parse(saved) : initialQuotes;
   });
 
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  useEffect(() => {
-    localStorage.setItem('quotes', JSON.stringify(quotes));
-  }, [quotes]);
-
-
-  const [history,setHistory] = useState([]);
 
   const handleNextQuoteClick = () => {
     const randomIndex = Math.floor(Math.random() * quotes.length);
-    setHistory(prev => [...prev, currentIndex]);
+    setHistory((prev) => [...prev, currentIndex]);
     setCurrentIndex(randomIndex);
   };
+
+  const [history, setHistory] = useState([]);
 
   const handlePreviousQuoteClick = () => {
     if (history.length === 0) return;
 
     const previousIndex = history[history.length - 1];
-  setCurrentIndex(previousIndex);
-  setHistory(prev => prev.slice(0, prev.length - 1));
-  }
+    setCurrentIndex(previousIndex);
+    setHistory((prev) => prev.slice(0, prev.length - 1));
+  };
 
   const handleLikeClick = () => {
     const updatedQuotes = quotes.map((quote, index) => {
@@ -44,18 +40,33 @@ function App() {
     setQuotes(updatedQuotes);
   };
 
+
+
+  useEffect(() => {
+    localStorage.setItem("quotes", JSON.stringify(quotes));
+  }, [quotes]);
+
+
+
+
+
+
+
   return (
     <div className="App">
       <QuoteCard quoteObj={quotes[currentIndex]} />
 
       <div className="btn-container">
-      <button onClick={handleLikeClick}>
-  <i className="fa-solid fa-thumbs-up"></i>
-</button>
+        <button onClick={handleLikeClick}>
+          <i className="fa-solid fa-thumbs-up"></i>
+        </button>
 
-        <button onClick={handlePreviousQuoteClick}><i class="fa-solid fa-left-long"></i></button>
-        <button onClick={handleNextQuoteClick}><i class="fa-solid fa-right-long"></i></button>
-       
+        <button onClick={handlePreviousQuoteClick}>
+          <i class="fa-solid fa-left-long"></i>
+        </button>
+        <button onClick={handleNextQuoteClick}>
+          <i class="fa-solid fa-right-long"></i>
+        </button>
       </div>
     </div>
   );
