fc7c4c3eac9fe95be7082ecc3494862f7bc5e2e0
Busra-Demirkesen <busrademirkesen2@gmail.com>
2025-05-08 14:27:26 +0200

    refactor: remove unused QuoteCard.css and integrate styles directly in QuoteCard component

--- quoterandom/src/components/QuoteCard/QuoteCard.jsx
+++ quoterandom/src/components/QuoteCard/QuoteCard.jsx
@@ -1,4 +1,4 @@
-import './QuoteCard.css';
+
 import { useContext } from 'react';
 import { QuotesContext } from "../../context/QuotesContext";
 
@@ -8,10 +8,10 @@ function QuoteCard() {
   const quoteObj = quotes[currentIndex];
 
   return (
-    <div className="quote-card">
-      <p className="quote-text">"{quoteObj.quote}"</p>
-      <p className="quote-author">- {quoteObj.author}</p>
-      <p className="quote-likes"><i className="fa-solid fa-heart"></i> {quoteObj.likeCount}</p>
+    <div className="border border-gray-300 p-5 rounded-lg bg-[#a89882] shadow-lg">
+      <p className="text-lg italic mb-2 text-white">"{quoteObj.quote}"</p>
+      <p className="font-bold mb-2 text-white">- {quoteObj.author}</p>
+      <p className="text-base text-white"><i className="fa-solid fa-heart"></i> {quoteObj.likeCount}</p>
     </div>
   );
 }
